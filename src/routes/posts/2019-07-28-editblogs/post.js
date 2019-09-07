b88ca953cb276ae62264c130be5b11b02477d813
fsalazarnearbpo <fsalazar@nearbpo.com>
2019-09-06 23:18:41 -0500

    removing editblog post to an early date

--- src/routes/posts/2019-07-28-editblogs/post.js
+++
@@ -1,7 +0,0 @@
-export default {
-  title: `How to Edit your post on React Blog`,
-  tags: ['react', 'navi'],
-  spoiler: "Learn how to add new posts and tweak the theme to your liking.",
-  getContent: () => import('./document.mdx'),
-  hidden: true,
-}
