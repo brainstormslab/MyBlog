fd25bd43fe1d3f1c61beb6b1cfac9398d1021660
fsalazarnearbpo <fsalazar@nearbpo.com>
2019-09-06 23:19:46 -0500

    editblogs add it again on an early date

---
+++ src/routes/posts/2019-06-28-editblogs/post.js
@@ -0,0 +1,7 @@
+export default {
+  title: `How to Edit your post on React Blog`,
+  tags: ['react', 'navi'],
+  spoiler: "Learn how to add new posts and tweak the theme to your liking.",
+  getContent: () => import('./document.mdx'),
+  hidden: true,
+}
