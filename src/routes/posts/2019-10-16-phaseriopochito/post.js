26aa5df68289a0ac5935ca6f844e1e93c43fe935
fsalazarnearbpo <fsalazar@nearbpo.com>
2019-10-25 19:58:55 -0500

    Adding more blog content

---
+++ src/routes/posts/2019-10-16-phaseriopochito/post.js
@@ -0,0 +1,7 @@
+export default {
+    title: `Game with Phaser Comments and Example`,
+    tags: ['Phaser.io', 'Typescript', 'Games'],
+    spoiler: "Steps of how to create your own game with phaser.io and some comments of common issues and how to deploy.",
+    getContent: () => import('./document.mdx'),
+    hidden: false,
+  }
---
+++ src/routes/posts/2019-10-21-fisherman-businessman/post.js
@@ -0,0 +1,7 @@
+export default {
+    title: `A history of why you should be happy on what you do instead of looking for money`,
+    tags: ['History', 'Health'],
+    spoiler: "A nice history about the Fisherman and the businessman and from my thought why this is something to have on mind wherever you work.",
+    getContent: () => import('./document.mdx'),
+    hidden: false,
+  }
