9dde30826030e84e0650fbb089f34b19d56994b8
fsalazarnearbpo <fsalazar@nearbpo.com>
2019-11-16 09:11:34 -0600

    updating name

--- src/siteMetadata.js
+++ src/siteMetadata.js
@@ -1,7 +1,7 @@
 export default {
   // The blog's title as it appears in the layout header, and in the document
   // <title> tag.
-  title: "BrainstormLabs",
+  title: "Brainstorm Labs",
 
   author: "Francisco Salazar",
   description: "Ideas and solutions of a Jack of all trades, master of none but oftentimes better than a master of one. Still looking for improving all trades.",
