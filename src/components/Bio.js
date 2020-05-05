419311e3f60894115c67c2053c171a1f12443833
fsalazarnearbpo <fsalazar@nearbpo.com>
2020-05-04 23:02:36 -0500

    Adding more Styles

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -14,12 +14,24 @@ function Bio(props) {
           ${styles.Bio}
           ${props.className || ''}
         `}>
+          <div className={`${styles.header}`}>
           <img src={photoURL} alt="Me" />
           <p>
             Ideas and solutions of a Jack of all trades, master of none but oftentimes better than a master of one. Still looking for improving all trades, by{' '}
             <a href="https://github.com/fsalazarnearbpo">Francisco Daniel Salazar Aguirre</a>.
             
           </p>
+          </div>
+          <div>
+            <p>
+            // TODO: Show a Phrase call by the API
+
+            Free REST API for Multiple Topic Phrases on Serverless
+            https://github.com/fdsalazaraguirre/dayphrases
+
+            https://dayphrases-api.brainstormlist.com/api/dayphrase/random
+            </p>
+          </div>
         </div>
     </div>
   )
